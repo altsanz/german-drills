c38cd12d7f95fe4dd274a3eb2b7357a37e0ccf6c
Alejandro Sanz <asanz91@gmail.com>
2023-08-01 19:46:05 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,151 @@
+import { Box, Button, ButtonGroup, Container, Heading } from "@chakra-ui/react";
+import React from "react";
+import "./App.css";
+
+type Preposition = {
+  preposition: string;
+  mode: "akk" | "dat" | "wechsel";
+};
+
+type Prepositions = ReadonlyArray<Preposition>;
+
+const prepositions: Prepositions = [
+  {
+    preposition: "bis",
+    mode: "akk",
+  },
+  {
+    preposition: "durch",
+    mode: "akk",
+  },
+  {
+    preposition: "für",
+    mode: "akk",
+  },
+  {
+    preposition: "gegen",
+    mode: "akk",
+  },
+  {
+    preposition: "ohne",
+    mode: "akk",
+  },
+  {
+    preposition: "um",
+    mode: "akk",
+  },
+  { preposition: "an", mode: "wechsel" },
+  { preposition: "auf", mode: "wechsel" },
+  // { preposition: "entlang", mode: "wechsel" },
+  { preposition: "hinter", mode: "wechsel" },
+  { preposition: "in", mode: "wechsel" },
+  { preposition: "neben", mode: "wechsel" },
+  { preposition: "über", mode: "wechsel" },
+  { preposition: "unter", mode: "wechsel" },
+  { preposition: "vor", mode: "wechsel" },
+  { preposition: "zwischen", mode: "wechsel" },
+  { preposition: "aus", mode: "dat" },
+  { preposition: "ausser", mode: "dat" },
+  { preposition: "bei", mode: "dat" },
+  // { preposition: "gegenüber", mode: "dat" },
+  { preposition: "mit", mode: "dat" },
+  { preposition: "nach", mode: "dat" },
+  { preposition: "seit", mode: "dat" },
+  { preposition: "von", mode: "dat" },
+  { preposition: "zu", mode: "dat" },
+];
+function App() {
+  const { preposition, nextPreposition } = usePreposition(prepositions);
+  const [right, setRight] = React.useState<boolean | null>(null);
+
+  const [response, setResponse] = React.useState<"akk" | "dat" | "wechsel">();
+  React.useEffect(() => {
+    if (response) {
+      setRight(response === preposition.mode);
+      setTimeout(() => {
+        nextPreposition();
+        setRight(null);
+        setResponse(undefined);
+      }, 2000);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [response]);
+
+  const answerColor =
+    right === null ? "gray.400" : right ? "green.400" : "red.400";
+
+  const getButtonColorScheme = (buttonValue: Preposition["mode"]) => {
+    if (right === null) return "gray";
+    if (buttonValue === preposition.mode) return "green";
+
+    if (!right && buttonValue === response) return "red";
+    return "gray";
+  };
+  return (
+    <Container>
+      <Box display={"flex"} flexDirection={"column"}>
+        <Box
+          flexGrow={1}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Box>
+            <Heading size="4xl" color={answerColor}>
+              {preposition.preposition}
+            </Heading>
+          </Box>
+        </Box>
+        <Box style={{ paddingBottom: "20px" }}>
+          <ButtonGroup flexGrow={0}>
+            <Button
+              colorScheme={getButtonColorScheme("akk")}
+              disabled={right !== null}
+              onClick={() => setResponse("akk")}
+            >
+              Akk
+            </Button>
+            <Button
+              colorScheme={getButtonColorScheme("wechsel")}
+              disabled={right !== null}
+              onClick={() => setResponse("wechsel")}
+            >
+              Wechsel
+            </Button>
+            <Button
+              colorScheme={getButtonColorScheme("dat")}
+              disabled={right !== null}
+              onClick={() => setResponse("dat")}
+            >
+              Dat
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
+
+export default App;
+
+const usePreposition = (
+  prepositions: Prepositions
+): {
+  preposition: Preposition;
+  nextPreposition(): void;
+} => {
+  const getRandomPreposition = () =>
+    prepositions[Math.trunc(Math.random() * prepositions.length)];
+
+  const [preposition, setPreposition] = React.useState<Preposition>(
+    getRandomPreposition()
+  );
+  const nextPreposition = () => {
+    setPreposition(getRandomPreposition());
+  };
+
+  return {
+    preposition,
+    nextPreposition,
+  };
+};
