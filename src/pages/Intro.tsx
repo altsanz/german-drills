678bd607f3bdfb71c31366d9bcaa713ae8cbb234
Alejandro Sanz <asanz91@gmail.com>
2023-08-03 23:13:35 +0200

    style a little bit intro page

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -2,21 +2,26 @@ import { Box, Button, Heading } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 export const Intro: React.FC = () => {
-  console.log("holaaa");
   const navigate = useNavigate();
   return (
     <Box
       style={{
+        height: "100%",
         display: "flex",
         flexDirection: "column",
-        justifyContent: "center",
         alignItems: "center",
+        paddingTop: "200px",
       }}
     >
-      <Heading as="h2" size="2xl" noOfLines={1}>
+      <Heading as="h2" size="2xl">
         German Drills
       </Heading>
-      <Button onClick={() => navigate("/wech-akk-dat")}>Akk Wech Dat</Button>
+      <Button
+        style={{ marginTop: "50px" }}
+        onClick={() => navigate("/wech-akk-dat")}
+      >
+        Akk Wech Dat
+      </Button>
     </Box>
   );
 };
