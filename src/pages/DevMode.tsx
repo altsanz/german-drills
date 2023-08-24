7a887aeda66699911446a5550353931c484e4214
Alejandro Sanz Díaz <asanz91@gmail.com>
2023-08-24 02:12:42 +0200

    add rest of logs to dev mode

--- src/pages/DevMode.tsx
+++ src/pages/DevMode.tsx
@@ -3,16 +3,34 @@ import React from "react";
 import ReactJson from "react-json-view";
 
 const DevMode: React.FC = () => {
-  const myJsonObject = React.useMemo(
+  const wechAkkDatJSON = React.useMemo(
     () => JSON.parse(localStorage.getItem("wech-akk-dat") ?? "{}"),
     []
   );
+
+  const wechAkkDatTurboJSON = React.useMemo(
+    () => JSON.parse(localStorage.getItem("wech-akk-dat-turbo") ?? "{}"),
+    []
+  );
+  const prepAndPronJSON = React.useMemo(
+    () => JSON.parse(localStorage.getItem("prep-and-pron") ?? "{}"),
+    []
+  );
+
   return (
     <Container height={"100%"}>
       <Heading as="h2" marginY={5} size="1xl">
         wech-akk-dat
       </Heading>
-      <ReactJson src={myJsonObject} />
+      <ReactJson src={wechAkkDatJSON} />
+      <Heading as="h2" marginY={5} size="1xl">
+        wechAkkDatTurboJSON
+      </Heading>
+      <ReactJson src={wechAkkDatTurboJSON} />
+      <Heading as="h2" marginY={5} size="1xl">
+      prepAndPronJSON
+      </Heading>
+      <ReactJson src={prepAndPronJSON} />
 
       <pre>{}</pre>
     </Container>
